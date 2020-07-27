d49bc698ef4f4f2d449724323d62f6786947b9d2
milan090 <milanmuhammed1@gmail.com>
2020-07-27 18:09:52 +0530

    Updates

---
+++ precache-manifest.462c5457da3dafe6405741123086a5a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cc323dea6e3650615ac8206cdc4b96",
+    "url": "/react-pomo-todos/index.html"
+  },
+  {
+    "revision": "4193f5ea03b09e99a6d9",
+    "url": "/react-pomo-todos/static/css/main.dca7eeea.chunk.css"
+  },
+  {
+    "revision": "ff97324276209c2ab2fe",
+    "url": "/react-pomo-todos/static/js/2.dc52b2da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomo-todos/static/js/2.dc52b2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4193f5ea03b09e99a6d9",
+    "url": "/react-pomo-todos/static/js/main.5eda62b3.chunk.js"
+  },
+  {
+    "revision": "61687a37780793803e61",
+    "url": "/react-pomo-todos/static/js/runtime-main.a2198064.js"
+  },
+  {
+    "revision": "f8b06e53ddbaa50e310bbf524c3271f2",
+    "url": "/react-pomo-todos/static/media/alarm.f8b06e53.mp3"
+  }
+]);
